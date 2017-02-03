dc4898c4819dae94dfa202978e3c6f613992d680
Manish Gautam <manish.gautam.india@gmail.com>
2017-02-03 09:02:13 +0530

    Integrated Modal box

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,12 +7,11 @@
 
   /** @ngInject */
   function MainController(toastr, $scope, $uibModal) {
+    
     $scope.orightml = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><img class="ta-insert-video" ta-insert-video="http://www.youtube.com/embed/2maA1-mvicY" src="" allowfullscreen="true" width="300" frameborder="0" height="250"/></p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li style="color: green;">Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li class="text-danger">Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE9+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p><h4>Supports non-latin Characters</h4>';
     $scope.htmlcontent = $scope.orightml;
     $scope.disabled = false;
-    var vm = this;
-    vm.showToastr = showToastr;
-    vm.hello = "Hello World";
+    
 
     var modalInstance = $uibModal.open({
         templateUrl: '/app/main/editor.html',
@@ -22,8 +21,6 @@
 
 
     $scope.open = function () {
-
-      
       modalInstance.result.then(function (selectedItem) {
         $scope.selected = selectedItem;
       });
@@ -32,11 +29,6 @@
     $scope.cancel = function () {
       modalInstance.dismiss('cancel');
     };
-
-
-    function showToastr() {
-      toastr.info('Hello Toaster');
-      vm.classAnimation = '';
-    }
+    
   }
 })();
